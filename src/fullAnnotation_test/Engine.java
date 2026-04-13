3c05f4bfec44446667cf64c5437bda66d7a49bbf
Sunny <sunnysinghhrana@gmail.com>
2026-04-13 23:23:16 +0530

    -create Main.java

--- src/fullAnnotation_test/Engine.java
+++ src/fullAnnotation_test/Engine.java
@@ -1,5 +1,16 @@
 package fullAnnotation_test;
 
-public class Engine {
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope("prototype")
+public class Engine {
+	public Engine(){
+		System.out.println("Engine created");
+	}
+	
+	public void start() {
+		System.out.println("Engine Started");
+	}
 }
