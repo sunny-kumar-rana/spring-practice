897a5664695a2f7eeb7e3996da91549388a7a154
Sunny <sunnysinghhrana@gmail.com>
2026-04-14 15:47:37 +0530

    -remove @Component from engine

--- src/fullAnnotation_test/Engine.java
+++ src/fullAnnotation_test/Engine.java
@@ -1,10 +1,5 @@
 package fullAnnotation_test;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("prototype")
 public class Engine {
 	public Engine(){
 		System.out.println("Engine created");
