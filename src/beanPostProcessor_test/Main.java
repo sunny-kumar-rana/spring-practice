322757dd99645f9223bf720c87fe898e955f2b7f
Sunny <sunnysinghhrana@gmail.com>
2026-04-15 23:08:05 +0530

    -create Main class

---
+++ src/beanPostProcessor_test/Main.java
@@ -0,0 +1,15 @@
+package beanPostProcessor_test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		context.getBean(Car.class);
+		context.getBean(Car.class);
+		context.getBean(Car.class);
+		context.getBean(Car.class);
+	}
+}
