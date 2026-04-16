6ebbd10a91742cc03376f4a2e924e34d20a6a4a9
Sunny <sunnysinghhrana@gmail.com>
2026-04-16 23:00:55 +0530

    -create AppConfig

---
+++ src/spring_task/AppConfig.java
@@ -0,0 +1,10 @@
+package spring_task;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring_task")
+public class AppConfig {
+	
+}
