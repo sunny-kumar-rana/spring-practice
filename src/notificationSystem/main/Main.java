dc2c8992709a98e5a8bb95be656e0ceba108ba4e
Sunny <sunnysinghhrana@gmail.com>
2026-04-21 23:42:15 +0530

    -create Main class ( executable logic )

---
+++ src/notificationSystem/main/Main.java
@@ -0,0 +1,19 @@
+package notificationSystem.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import customerOrders.springConfig.AppConfig;
+import notificationSystem.dispatcher.NotificationDispatcher;
+import notificationSystem.model.Message;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(notificationSystem.config.AppConfig.class);
+		
+		NotificationDispatcher dispatcher = context.getBean(NotificationDispatcher.class);
+		
+		dispatcher.dispatch(new Message(121, " hello there. wassup?", "suresh"), "email");
+		
+	}
+}
