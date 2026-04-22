4dd666d95ecc68a676c57dce988cde2ce3794be7
Sunny <sunnysinghhrana@gmail.com>
2026-04-22 23:54:39 +0530

    -add fallback logic in enum

--- src/notificationSystem/dispatcher/NotificationType.java
+++ src/notificationSystem/dispatcher/NotificationType.java
@@ -2,7 +2,30 @@ package notificationSystem.dispatcher;
 
 public enum NotificationType {
 	
-	EMAIL,
-	SMS,
-	PUSH
+	EMAIL("EMAIL") {
+		public NotificationType next() {
+			return SMS;
+		}
+	},
+	SMS("SMS") {
+		public NotificationType next() {
+			return PUSH;
+		}
+	},
+	PUSH("PUSH") {
+		public NotificationType next() {
+			return null;
+		}
+	};
+	
+	private final String beanName;
+	
+	private NotificationType(String beanName) {
+		this.beanName = beanName;
+	}
+	public String getBeanName() {
+		return this.beanName;
+	}
+	
+	public abstract NotificationType next();
 }
