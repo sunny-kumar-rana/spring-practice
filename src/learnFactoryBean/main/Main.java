3d12ce21d8ccc99357f8da41370690a91d4ff72d
Sunny <sunnysinghhrana@gmail.com>
2026-04-28 22:39:16 +0530

    -create Main class

---
+++ src/learnFactoryBean/main/Main.java
@@ -0,0 +1,18 @@
+package learnFactoryBean.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import learnFactoryBean.config.AppConfig;
+import learnFactoryBean.engine.Engine;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Engine engine = context.getBean("engine", Engine.class);
+		
+		engine.start();
+//		engine.getClass();
+	}
+}
