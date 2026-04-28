40670cc735fc1f4b6f4af43bb2d45247e1d15548
Sunny <sunnysinghhrana@gmail.com>
2026-04-28 22:39:03 +0530

    -create AppConfig class

---
+++ src/learnFactoryBean/config/AppConfig.java
@@ -0,0 +1,10 @@
+package learnFactoryBean.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("learnFactoryBean")
+public class AppConfig {
+
+}
