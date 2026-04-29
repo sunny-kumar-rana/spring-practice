e93a6d14bc7abca12b66045f0c8ef57cee95fb45
Sunny <sunnysinghhrana@gmail.com>
2026-04-29 22:14:27 +0530

    -update Main class ( call Notification bean)

--- src/learnFactoryBean/main/Main.java
+++ src/learnFactoryBean/main/Main.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import learnFactoryBean.config.AppConfig;
 import learnFactoryBean.engine.Engine;
+import learnFactoryBean.notification.Notification;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		Engine engine = context.getBean("engine", Engine.class);
-		
-		engine.start();
+//		Engine engine = context.getBean("engine", Engine.class);
+//		
+//		engine.start();
 //		engine.getClass();
+		
+		Notification notification = context.getBean("notification",Notification.class);
+		notification.show();
 	}
 }
